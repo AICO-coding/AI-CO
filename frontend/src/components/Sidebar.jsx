675cc8ac7d5ad5317f369c7d6c052c891e7c7713
Kim-Yun-Seo <rlagrace410@gmail.com>
2026-04-28 17:19:46 +0900

    [Style] 코드 공백 변경

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
 function Sidebar() {
-    return (
-    <div style={{ width: "250px", background: "#f5f5f5" }}>
-        <h2>CodeDojo</h2>
+  return (
+  <div style={{ width: "250px", background: "#f5f5f5" }}>
+    <h2>CodeDojo</h2>
 
-        <nav>
-            <div><Link to="/">홈</Link></div>
-            <div><Link to="/track">학습 트랙</Link></div>
-            <div><Link to="/daily">데일리 태스크</Link></div>
-        </nav>
-    </div>
-    );
+    <nav>
+      <div><Link to="/">홈</Link></div>
+      <div><Link to="/track">학습 트랙</Link></div>
+      <div><Link to="/daily">데일리 태스크</Link></div>
+    </nav>
+  </div>
+  );
 }
 
 export default Sidebar;
