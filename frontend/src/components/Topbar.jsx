822ae91ed489f5660e75d11988bd995da6c177c7
Kim-Yun-Seo <rlagrace410@gmail.com>
2026-05-27 16:13:16 +0900

    [Feat] Topbar 로고 /home 이동

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,13 +1,32 @@
 import "../styles/Topbar.css";
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+
+import { useNavigate } from "react-router-dom";
 
 function Topbar() {
+
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/home");
+  };
+
   return (
     <div id="topbar">
-      <div className="tb-logo">
-        <img src={logo} alt="로고" className="tb-logo-img" />
+
+      <div
+        className="tb-logo"
+        onClick={goHome}
+      >
+        <img
+          src={logo}
+          alt="로고"
+          className="tb-logo-img"
+        />
+
         AI<span>CO</span>
       </div>
+
     </div>
   );
 }
